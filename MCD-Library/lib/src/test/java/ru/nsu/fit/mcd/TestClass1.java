d7c186e6ed28a1437f009c6bbcc975b3e1908f92
TuS <tussss89@gmail.com>
2022-10-19 19:06:17 +0700

    refactoring

--- MCD-Library/lib/src/test/java/ru/nsu/fit/mcd/TestClass1.java
+++ MCD-Library/lib/src/test/java/ru/nsu/fit/mcd/TestClass1.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 public class TestClass1 {
 
-        public int intField;
+//        public int intField;
+//
+//        public double doubleField;
+//
+//        public Object objectField;
+       //public TestClass2<String[], Double>[] arrayField;
 
-        public double doubleField;
-
-        public Object objectField;
-
-        public TestClass2<String[], Double>[] arrayField;
-
-        //public List<TestClass2<String, Object>> list;
+       public List<Integer> list;
+       public List<TestClass2<String[], Double>> genericClassList;
 }
